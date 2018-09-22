e62a491902d082e25a9e9d9d84572eb63466fec5
Gabriel Humberto <Helmsdeephk@live.com>
2018-09-22 00:27:16 -0300

    APP CEP CONCLUIDO

---
+++ TCCTESTE2109/TCC/TCC/TCC/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TCC.Servico.Modelo;
+using TCC.Servico;
+
+
+namespace TCC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCep;
+
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+
+            //TODO - Validações.
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = viaCepservico.BuscarEnderecoViaCEP(cep);
+
+                    if(end != null)
+                    {
+                        RESULTADO.Text = string.Format("CEP: {0}, Cidade: {1} {2}, Endereço: {3}", end.cep, end.localidade, end.uf, end.logradouro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + cep, "OK");
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+                }
+                
+            }
+            
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve conter 8 caracteres.", "OK");
+
+                valido = false;
+            }
+
+            int NovoCEP = 0;
+
+            if(!int.TryParse(cep, out NovoCEP))
+            {
+                DisplayAlert("ERRO", "CEP inválido! O CEP deve ser composto apenas por números.", "OK");
+
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ TCCTESTE2109/TCC/TCC/TCC/Servico/viaCepservico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using TCC.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace TCC.Servico
+{
+    public class viaCepservico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+            
+            return end;
+        }
+    }
+}
