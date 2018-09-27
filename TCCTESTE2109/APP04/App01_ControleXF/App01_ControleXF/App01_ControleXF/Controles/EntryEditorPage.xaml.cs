7cd80d5ce6b48b71ff46a77253da2dd00f84b52a
Gabriel Humberto <Helmsdeephk@live.com>
2018-09-27 13:55:45 -0300

    TCC V0.1

---
+++ TCCTESTE2109/APP04/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/EntryEditorPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EntryEditorPage : ContentPage
+	{
+		public EntryEditorPage ()
+		{
+			InitializeComponent ();
+            TxtIdade.TextChanged += delegate (object sender, TextChangedEventArgs args)
+            {
+                Lbl_Duplicado.Text = args.NewTextValue;
+            };
+
+            Txt_Comentario.Completed += delegate (object sender, EventArgs args)
+            {
+                LblQuantidadeCaracteres.Text = Txt_Comentario.Text.Length.ToString();
+            };
+		}
+	}
+}
---
+++ TCCTESTE2109/TCCFINAL/TCCFINAL/TCCFINAL/MenuMaster/MenuMaster.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TCCFINAL.MenuMaster
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuMaster : MasterDetailPage
+	{
+		public MenuMaster ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoToMainPage(object sender, EventArgs args)
+        {
+            Detail = new Paginas.MainPage();
+        }
+        private void GoToPagina1(object sender, EventArgs args)
+        {
+            Detail = new Paginas.Pagina1();
+        }
+        private void GoToPagina2(object sender, EventArgs args)
+        {
+            Detail = new Paginas.Pagina2();
+        }
+        private void GoToPagina3(object sender, EventArgs args)
+        {
+            Detail = new Paginas.Pagina3();
+        }
+
+    }
+}
